1503466fba66cf594c447e74d85a7e6465d3cb1e
Szymon Sidor <szymon.sidor@gmail.com>
2015-03-01 18:34:33 -0500

    Most recent changes - who knows what it is?

--- cv_data.py
+++ cv_data.py
@@ -10,7 +10,7 @@ phone='+14438799664'
 facebook=r'fb.com/szymon.sidor'
 
 mit = {
-    'fixup': str(3.0),
+    'fixup': str(3.5),
     'when': r'January 2014 - Current',
     'where': r'Massachusetts Institute of Technology',
     'role': r'Computer Science PhD',
@@ -22,11 +22,11 @@ mit = {
         'Computer and Network Security (A)',
     ],
     'projects': {
-        r'Android Security' : r'covert video capture (\url{https://www.youtube.com/watch?v=sDzs6y4JVok})',
-        r'Policy search and unknown dynamics': r"Implementation of Sergey Levine's work which uses GMMs and linear approximation to estimate robot dynamics using small number of runs of the hardware. It uses estimated dynamics to optimize goal function for black box policy (in my case RNN). Report available online \url{https://www.dropbox.com/s/1e4hm4eidztvxu6/6.867-project.pdf?dl=0}",
+        r'Android Security' : r'Covert video capture. Demo available at \url{https://www.youtube.com/watch?v=sDzs6y4JVok}.',
+        r'Policy search and unknown dynamics': r"Implementation of Sergey Levine's work which uses GMMs and linear approximation to estimate robot dynamics using small number of runs of the hardware. It uses estimated dynamics to optimize goal function for black box policy (in my case RNN). Report available online \url{https://www.dropbox.com/s/1e4hm4eidztvxu6/6.867-project.pdf?dl=0}.",
     },
     'extra_info': {
-        'Research Interests': 'My research concerns applying machine learning techniques for Dialog Systems (think Siri solving more complex problems like planning your day). Specifically I am interested in text generation using modern machine learning techniques (RNNs, NTMs etc.). My academic adviser is Brian Williams.',
+        'Research Interests': 'My research concerns applying machine learning techniques for Dialog Systems (e.g. Siri-style assistant solving complex problems like planning your day). Specifically I am interested in text generation using modern machine learning techniques (RNNs, NTMs etc.). My academic adviser is Brian Williams.',
         'Research Assistantship': 'My PhD is sponsored by TATA Center at MIT. Part of my research involves traveling to rural areas in India and deploying my algorithms on village scale power microgrid.',
     },
 }
@@ -66,25 +66,64 @@ google1 = {
     'role': 'Software Engineering Intern',
     'where': 'Google',
     'image' : 'google.png',
-    'description': "Working on features for Chrome Operating System"
+    'description': "Worked on features for Chrome Operating System. Implemented flash media formatting, unformatted media detection, screen saver, alternative login methods (OpenID). Investigated potential memory allocation improvements."
 }
 
 cambridge = {
-    'fixup': str(0.0),
+    'fixup': str(2.2),
     'when': r'October 2010 - June 2013',
     'where': r'University of Cambridge',
     'role': r'Computer Science Tripos',
     'image': 'cambridge.png',
     'courses': [
-        'Siema',
-        'Siema',
-        'Siema',
-        'Siema',
-
-    ]
+       'Concurrent and Distributed Systems',
+       'Discrete Mathematics',
+       'Digital Signal Processing',
+       'Operating Systems',
+       'Hoare Logic and Formal Proofs',
+       'Compiler Optimization',
+       'Natural Language Processing',
+    ],
+    'competitions': [
+        'Facebook Hacker Cup 2013 (210th place)',
+        'ACM ICPC qualification round NWERC 2011 (Bronze medal)',
+        'VK cup 2012 (193th place)',
+    ], 'extra_info': {
+        'Overall grades': 'First, second and third year respectively: 1, 1, 2.1',
+        'Final year project': r'Springboard - cryptographically secure distributed social network over opportunistic communication supervised by prof. Jon Crowcroft. Report available online \url{https://www.dropbox.com/s/wu9e9jzuvb97die/dissertation-final.pdf?dl=0}',
+        'Cambridge Algorithms Society': 'I was the founding member of CAS - the mission was to introduce students to programming competitions. We were also in charge of organizing university-level qualifications for ICPC.'
+    }
 }
 
-events = [mit, dropbox, google2, google1, cambridge]
+highschool = {
+    'fixup': str(1.0),
+    'when': r'September 2007 - June 2010',
+    'where': r'III Highschool in Gdynia, Poland',
+    'role': r'Student',
+    'image': '3lo.png',
+    'competitions': [
+        'Polish Olympiad in Computer Science (25th place)',
+        'TopCoder Yellow',
+        r'ACM ICPC qualifications CEPC 2009 (1/8 highschools, 20/80 highschools + universities)',
+        'Polish Olympiad in Mathematics (twice honorable mentioned)',
+    ],
+    'description': r"During highschool I sent two months every year at computer science camps organized by Stowarzyszenie Talent. I contributed problems, gave talks and in general helped organize those events."
+}
+
+life = {
+    'fixup': str(1.0),
+    'when': r'30 January 1991 - Current',
+    'where': r'Poland, UK, USA and sometimes other places',
+    'role': r'Life',
+    'image': 'life.jpg',
+    'extra_info': {
+        'Hobbies': r"Programming Competitions, Sailing, Snowboarding, Martial Arts, Parkour (\url{http://goo.gl/rz3a9}), board games and reading books",
+        'My blog': r'\url{http://snacksforyourmind.blogspot.com}',
+        'Languages': r"English (fluent), Polish (native), Spanish (experimenting with Duolingo)"
+    }
+}
+
+events = [mit, dropbox, google2, google1, cambridge, highschool, life]
 
 # DON'T EDIT BELOW THIS LINE
 first_names = ' '.join(name.split(' ')[:-1])
